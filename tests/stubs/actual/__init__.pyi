f023dbb8288da260bbf6f3fcd80d4a5143845a42
Aleksei Maslakov <lesha.maslakov@gmail.com>
2019-05-21 21:35:18 +0300

    Print content of mismatched files

---
+++ tests/stubs/actual/__init__.pyi
@@ -0,0 +1,8 @@
+from dataclasses import dataclass
+from enum import IntEnum
+from typing import *
+
+from . import dates
+from . import shared
+from . import todo_v2
+from . import todo
--- tests/test_main.py
+++ tests/test_main.py
@@ -5,12 +5,15 @@ from thriftpyi.main import thriftpyi
 
 
 def test_thriftpyi():
-    thriftpyi("example/interfaces", "tests/stubs/actual/")
+    output_dir = "tests/stubs/actual"
+    thriftpyi("example/interfaces", output_dir)
     pyi_files = ["__init__.pyi", "dates.pyi", "shared.pyi", "todo.pyi", "todo_v2.pyi"]
     match, mismatch, errors = filecmp.cmpfiles(
-        "tests/stubs/actual", "tests/stubs/expected", pyi_files
+        output_dir, "tests/stubs/expected", pyi_files
     )
     assert errors == []
-    assert mismatch == []
+    assert mismatch == [], "\n\n".join(
+        [open(f"{output_dir}/{file}").read() for file in mismatch]
+    )
     assert match == pyi_files
     shutil.rmtree("tests/stubs/actual/")
