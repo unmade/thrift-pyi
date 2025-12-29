6c082f34484bb942c7c0c5b2a206d383a8686834
Stuart Axelbrooke <stuart@yobi.ai>
2025-12-29 12:49:45 -0800

    Fix for `StopIteration` on empty const collections (#62)

--- example/interfaces/shared.thrift
+++ example/interfaces/shared.thrift
@@ -7,6 +7,9 @@ exception EmptyException {}
 const i32 INT_CONST_1 = 1234
 const map<string,string> MAP_CONST = {"hello": "world", "goodnight": "moon"}
 const i32 INT_CONST_2 = 1234
+const list<string> EMPTY_LIST = []
+const map<string,i32> EMPTY_MAP = {}
+const set<i32> EMPTY_SET = []
 
 struct LimitOffset {
     1: optional i32 limit
