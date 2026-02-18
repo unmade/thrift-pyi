7aaf011e608243858c000104dec6ba1d7f9174cb
Frank Portman <frank1214@gmail.com>
2026-02-18 14:19:10 -0500

    more tests

--- tests/stubs/expected/cross_dir/child.pyi
+++ tests/stubs/expected/cross_dir/child.pyi
@@ -1,7 +1,14 @@
 from dataclasses import dataclass
+from enum import IntEnum
 from . import _typedefs
+
+class Status(IntEnum):
+    ACTIVE = 1
+    INACTIVE = 2
 
 @dataclass
 class Identifier:
     value: _typedefs.String
     type_id: _typedefs.I32
+
+DEFAULT_ID: Identifier = Identifier(value="unknown", type_id=0)
