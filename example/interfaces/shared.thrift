822c68a7d8c63ad3f6c9e92d2d92717065d4c624
Aleksei Maslakov <lesha.maslakov@gmail.com>
2026-03-23 20:44:20 +0100

    Support empty collections as `const` values

--- example/interfaces/shared.thrift
+++ example/interfaces/shared.thrift
@@ -8,6 +8,10 @@ const i32 INT_CONST_1 = 1234
 const map<string,string> MAP_CONST = {"hello": "world", "goodnight": "moon"}
 const i32 INT_CONST_2 = 1234
 
+const list<string> EMPTY_LIST = []
+const map<string,i32> EMPTY_MAP = {}
+const set<i32> EMPTY_SET = []
+
 struct LimitOffset {
     1: optional i32 limit
     2: optional i32 offset
